52a8845b624e930572947baf12dd76d69f99ca81
Michael <michael.mingdao.li@gmail.com>
2023-10-14 19:16:39 -0700

    Add android and ios folders with capacitor camera functionality configured

---
+++ android/app/src/main/java/com/snipcal/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.snipcal.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,14 +22,9 @@ const Home: React.FC = () => {
       resultType: CameraResultType.Uri
     });
   
-    // image.webPath will contain a path that can be set as an image src.
-    // You can access the original file using image.path, which can be
-    // passed to the Filesystem API to read the raw data of the image,
-    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    // image.webPath will contain a path that can be set as an image src
+    // You can access the original file using image.path
     const imageUrl = image.webPath;
-  
-    // Can be set to the src of an image now
-    console.log("Image taken at: " + imageUrl);
   };
 
   const hardCodedEvents = [
